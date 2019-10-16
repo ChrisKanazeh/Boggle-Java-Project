85767fb2dd43f22bfc4c77273006ee5ecb5dd492
ChrisKanazeh <56492774+ChrisKanazeh@users.noreply.github.com>
2019-10-16 07:18:37 -0400

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,85 @@
+/*
+Christopher Kanazeh
+Boggle Project Assignment 1
+6/19/2015
+Professor Whiting
+COP 3330
+ */
+//package
+package core;
+//import
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Chris
+ */
+//public class
+public class Board {
+    //set constants 
+    private final int NUMBER_OF_DICE = 16;
+    private final int NUMBER_OF_SIDES = 6;
+    private final int GRID = 4;
+    
+    //stores all dice data
+    ArrayList boggleData = new ArrayList();
+    //stores all die data
+    ArrayList<Die> boggleDice = new ArrayList<Die>();
+    
+    //stores all boggle data
+    public Board(ArrayList inData)
+    {
+        
+        boggleData = inData;
+    }
+    //populate data function
+    private void populateDice()
+    {
+        //declare variable
+        Die die;
+        //loop through the dice
+        for(int dice = 0; dice < NUMBER_OF_DICE; dice++)
+        {
+            //declare variable
+            die = new Die();
+            //loop through the sides
+            for(int side = 0; side < NUMBER_OF_SIDES; side++)
+            {
+                //add the letters to the die
+                die.addLetter(boggleData.get((dice * 6) + side).toString());
+                
+            }
+            //prints out the data
+            System.out.print("Die " + dice + ": ");
+            die.displayAllLetters();
+            System.out.println();
+            //adds each die instance to the arraylist 
+            boggleDice.add(die);
+        }
+    }
+    
+    //shake dice function
+    public ArrayList shakeDice()
+    {
+        //counter
+        int counter = 0;
+        //populate dice call
+        populateDice();
+        //prints out the word boggle board
+        System.out.println("Boggle board ");
+        //loop through each dice
+        for(Die die : boggleDice)
+        {
+            //increment counter by 1
+            counter ++;
+            //prints the letter for the grid
+            System.out.print(die.getLetter() + " ");
+            //prints the next line if mod equals 0
+            if(counter % GRID == 0)
+                System.out.println();
+        }
+        //returns boggle dice
+        return boggleDice;
+    }
+    
+}
---
+++ Boggle.java
@@ -0,0 +1,66 @@
+/*
+Christopher Kanazeh
+Boggle Project Assignment 1
+6/19/2015
+Professor Whiting
+COP 3330
+ */
+
+//package
+package boggle;
+//imports
+import core.Board;
+import inputOutput.ReadDataFile;
+//import java.io.File;
+//import java.net.URL;
+import java.util.ArrayList;
+import userInterface.BoggleUi;
+
+
+
+/**
+ *
+ * @author Chris
+ */
+//public class
+public class Boggle 
+{
+    
+    //array to store the data, string to set the name of the input file
+    private static ArrayList boggleData = new ArrayList();
+    private static String dataFileName = new String("BoggleData.txt");
+    private static String dictionaryFileName = new String("TemporaryDictionary.txt");
+    
+    /**
+     * @param args the command line arguments
+     */
+    //main function
+    public static void main(String[] args) 
+    {
+        //ArrayList dataFile;
+        
+        // TODO code application logic here
+        //ReadDataFile call
+        ReadDataFile data = new ReadDataFile(dataFileName);
+        //call to populate data
+        data.populateData();
+        //boggle data set to get the data
+            //CHANGED THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //boggleData = data.getData();
+        
+        ReadDataFile dictionary = new ReadDataFile(dictionaryFileName);
+        dictionary.populateData();
+        
+        //sets the board equal to the board of boggle data
+            //CHANGED THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //Board board = new Board(boggleData);
+        Board board = new Board(data.getData());
+        //shake dice call
+        //board.shakeDice();
+        //boggleui call
+        //BoggleUi boggleUi = new BoggleUi(board); 
+        BoggleUi boggleUi = new BoggleUi(board, dictionary.getData());
+        
+    }
+    
+}
